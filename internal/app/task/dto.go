647717d9783e0e522cd027347c90769bee21a3e3
Igor Meshkov <meshkov-74@yandex.ru>
2025-10-29 17:58:01 +0500

    added dtos

--- internal/app/task/dto.go
+++ internal/app/task/dto.go
@@ -1,1 +1,80 @@
 package task
+
+import (
+	"time"
+)
+
+// Task -------------------------------------------------------------------------------------
+type GetTaskShortResponse struct {
+	ID        string  `json:"id"`
+	Title     string  `json:"title"`
+	TargetBPM int     `json:"target_bpm"`
+	Progress  float64 `json:"progress"`
+}
+
+type GetTaskResponse struct {
+	ID        string               `json:"id"`
+	Title     string               `json:"title"`
+	TargetBPM int                  `json:"target_bpm"`
+	Sessions  []GetSessionResponse `json:"sessions"`
+	Media     []GetMediaResponse   `json:"media"`
+	Links     []GetLinkResponse    `json:"links"`
+}
+
+type SaveTaskRequest struct {
+	Title     string `json:"title" validate:"required,min=1,max=50"`
+	TargetBPM int    `json:"target_bpm" validate:"required,number"`
+}
+
+// Session -------------------------------------------------------------------------------------
+type GetSessionResponse struct {
+	ID         string    `json:"id"`
+	BPM        int       `json:"bpm"`
+	Note       string    `json:"note"`
+	Confidence int       `json:"confidence"`
+	StartTime  time.Time `json:"start_time"`
+	EndTime    time.Time `json:"end_time"`
+	Duration   int       `json:"duration"`
+}
+
+type SaveSessionRequest struct {
+	BPM        int       `json:"bpm" validate:"required,number"`
+	Note       string    `json:"note"`
+	Confidence int       `json:"confidence" validate:"required,number,min=1,max=5"`
+	StartTime  time.Time `json:"start_time" validate:"required"`
+	EndTime    time.Time `json:"end_time" validate:"required"`
+}
+
+// Media -------------------------------------------------------------------------------------
+type GetMediaResponse struct {
+	ID        string    `json:"id"`
+	Type      string    `json:"type"`
+	Filename  string    `json:"filename"`
+	URL       string    `json:"url"`
+	Size      int64     `json:"size"`
+	Duration  int       `json:"duration"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type SaveMediaRequest struct {
+	Type     string `json:"type" validate:"required"`
+	Filename string `json:"filename" validate:"required,min=1"`
+	URL      string `json:"url" validate:"required,url"`
+	Size     int64  `json:"size" validate:"required,number"`
+	Duration int    `json:"duration" validate:"required,number"`
+}
+
+// Link -------------------------------------------------------------------------------------
+type GetLinkResponse struct {
+	ID        string    `json:"id"`
+	URL       string    `json:"url"`
+	Title     string    `json:"title"`
+	Type      string    `json:"type"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type SaveLinkRequest struct {
+	URL   string `json:"url" validate:"required,url"`
+	Title string `json:"title" validate:"required,min=1,max=50"`
+	Type  string `json:"type" validate:"required,min=1,max=50"`
+}
