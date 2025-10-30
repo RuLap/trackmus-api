e8e79b1bf85a758441ef522bd86f2a6bcee874cd
Igor Meshkov <meshkov-74@yandex.ru>
2025-10-30 20:40:46 +0500

    additional remove of url

--- migrations/00005_add_links_table.sql
+++ migrations/00005_add_links_table.sql
@@ -3,7 +3,6 @@
 CREATE TABLE IF NOT EXISTS "links" (
     "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     "task_id" UUID REFERENCES tasks(id) ON DELETE CASCADE,
-    "url" TEXT,
     "title" VARCHAR(50),
     "type" VARCHAR(50),
     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
