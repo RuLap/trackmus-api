ab80607872aba68cff9f8bf12c5c90be73017fbb
Igor Meshkov <meshkov-74@yandex.ru>
2025-10-30 20:31:34 +0500

    added task update

--- internal/app/task/task_repository.go
+++ internal/app/task/task_repository.go
@@ -12,6 +12,7 @@ type TaskRepository interface {
 	Get(ctx context.Context, userID uuid.UUID, isCompleted bool) ([]Task, error)
 	GetByID(ctx context.Context, id uuid.UUID) (*Task, error)
 	Create(ctx context.Context, task *Task) (*Task, error)
+	Update(ctx context.Context, task *Task) (*Task, error)
 }
 
 type taskRepository struct {
@@ -22,14 +23,14 @@ func NewTaskRepository(pool *pgxpool.Pool) TaskRepository {
 	return &taskRepository{pool}
 }
 
-func (r *taskRepository) Get(ctx context.Context, userID uuid.UUID, isActive bool) ([]Task, error) {
+func (r *taskRepository) Get(ctx context.Context, userID uuid.UUID, isCompleted bool) ([]Task, error) {
 	query := `
 		SELECT id, title, target_bpm, is_completed, created_at
 		FROM tasks
 		WHERE user_id = $1 AND is_completed = $2
 	`
 
-	rows, err := r.pool.Query(ctx, query, userID, isActive)
+	rows, err := r.pool.Query(ctx, query, userID, isCompleted)
 	if err != nil {
 		return nil, fmt.Errorf("database query failed: %w", err)
 	}
@@ -101,6 +102,30 @@ func (r *taskRepository) Create(ctx context.Context, task *Task) (*Task, error) 
 	}
 
 	task.ID = id
+
+	return task, nil
+}
+
+func (r *taskRepository) Update(ctx context.Context, task *Task) (*Task, error) {
+	query := `
+		UPDATE tasks
+		SET title = $2,
+			target_bpm = $3,
+			is_completed = $4
+		WHERE id = $1
+	`
+
+	_, err := r.pool.Exec(
+		ctx,
+		query,
+		task.ID,
+		task.Title,
+		task.TargetBPM,
+		task.IsCompleted,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update task: %w", err)
+	}
 
 	return task, nil
 }
